51bed4655eb2ba451eb31920c04ab03eb7361c1a
j3ongjisu <132436950+j3ongjisu@users.noreply.github.com>
2023-05-22 18:09:59 +0900

    Add files via upload

---
+++ js/project02.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.main_slide').slick({
+        //arrows: false,
+        fade: true,
+        //vertical: true,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+})
