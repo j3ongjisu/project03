564c2dd09fca73f157967d4c971030bd24f9feb2
j3ongjisu <132436950+j3ongjisu@users.noreply.github.com>
2023-05-23 01:08:08 +0900

    Add files via upload

--- js/project02.js
+++ js/project02.js
@@ -1,10 +1,38 @@
+//section1 메인 슬라이드
 $(function () {
     $('.main_slide').slick({
         //arrows: false,
         fade: true,
         //vertical: true,
+        dots: true,
         autoplay: true,
+        autoplaySpeed: 4500,
         pauseOnHover: false,
         pauseOnFocus: false,
     });
+})
+
+//section2 제품 슬라이드
+$(function () {
+    $('.product_slide').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    });
+
+    $('#product .arrows .left').on('click', function () {
+        $('.product_slide').slick('slcikPrev');
+    })
+    $('#product .arrows .right').on('click', function () {
+        $('.product_slide').slick('slcikNext');
+    });
+
 })
